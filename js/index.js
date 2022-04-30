9290a6c6688be132cfcbe4b9a191358c320b086b
upc <iupc100@gmail.com>
2022-04-30 09:43:54 -0500

    Empezar a aprender javascript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let nombre="Ulises";
+console.log(nombre);
+let edad=35;
+console.log("Mi nombre es "+nombre+" mi edad es "+edad);
+console.log(`Mi nombre es ${nombre} mi edad es ${edad}`); 
+let apellido="Pérez";
+var apellido1="Pérez";
+console.log(Window);
+var alert="estoy sobreescribiendo la función alert";
