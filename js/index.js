60f617e21bffbec4417e6eb6828a78e7ee85743a
upc <iupc100@gmail.com>
2022-05-01 08:30:35 -0500

    Tipos de datos en Javascript

--- js/index.js
+++ js/index.js
@@ -7,3 +7,25 @@ let apellido="Pérez";
 var apellido1="Pérez";
 console.log(Window);
 var alert="estoy sobreescribiendo la función alert";
+
+let caja1;
+console.log(caja1);
+let caja2=2*nombre;
+console.log(caja2);
+let caja3=null;
+console.log(caja3);
+let caja4=1+"1";
+console.log(caja4);
+console.log(typeof caja4);
+
+let caja5=["perro",23,caja1];
+console.log(caja5[2]);
+let persona={
+    nombre:"Ulises",
+    edad:35,
+    casado:true,
+    plantas:["albahaca", "tomate", "oregano"]
+};
+persona.plantas[3]="lavanda";
+console.log(persona);
+console.log(persona.persona[3]);
