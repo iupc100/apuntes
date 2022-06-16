dddfd6bbc94a2955d40aff61ab4473085dd7f3ff
upc <iupc100@gmail.com>
2022-06-16 08:39:51 -0500

    resuelto problemas de validaciÃon

--- Ejercicios_varios/ValidacionJs/js/index.js
+++ Ejercicios_varios/ValidacionJs/js/index.js
@@ -7,7 +7,7 @@ const expresiones={
     password: /^.{4,12}$/, // el punto significa todos los símbolos 
  
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7-14}$/  
+    telefono: /^\d{7,14}$/  
 }
 
 const validarFormulario=(e)=>{
@@ -16,19 +16,19 @@ const validarFormulario=(e)=>{
             validarCampo(expresiones.usuario,e.target,'usuario');
          break; 
          case 'nombre':
-            validarCampo(expresiones.usuario,e.target,'nombre');
+            validarCampo(expresiones.nombre,e.target,'nombre');
          break;  
          case 'password':
-            validarCampo(expresiones.usuario,e.target,'password');
+            validarCampo(expresiones.password,e.target,'password');
          break;  
          case 'password2':
-            validarCampo(expresiones.usuario,e.target,'password2');
+            validarPassword2();
          break;
          case 'correo':
-            validarCampo(expresiones.usuario,e.target,'correo');
+            validarCampo(expresiones.correo,e.target,'correo');
          break;   
          case 'telefono':
-            validarCampo(expresiones.usuario,e.target,'telefono');
+            validarCampo(expresiones.telefono,e.target,'telefono');
          break;
                 
     }
@@ -53,6 +53,28 @@ const validarCampo=(expresion,input,campo)=>{
 
     }
 
+}
+
+const validarPassword2=()=>{
+    const inputPassword1=document.getElementById('password');
+    const inputPassword2=document.getElementById('password2');
+        if (inputPassword1.value !== inputPassword2.value) {
+            document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+            document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+            document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+            document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+                        
+            
+        }
+        else{
+            document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+            document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+            document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+            document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+
+        }
 }
 
 /*
