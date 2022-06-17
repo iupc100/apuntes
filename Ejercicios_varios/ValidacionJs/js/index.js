3dcdfbf70ad0a31f4c4a874d0a76c0d2f1aeb737
upc <iupc100@gmail.com>
2022-06-17 07:55:19 -0500

    Terminando formulario validacion

--- Ejercicios_varios/ValidacionJs/js/index.js
+++ Ejercicios_varios/ValidacionJs/js/index.js
@@ -10,8 +10,16 @@ const expresiones={
     telefono: /^\d{7,14}$/  
 }
 
+const campos={          // se crea un objeto que por default son falsos
+    usuario:false,
+    nombre:false,
+    password:false,
+    correo:false,
+    telefono:false
+}
+
 const validarFormulario=(e)=>{
-    switch (e.target.name) {
+    switch (e.target.name) {        // se llama a la funcion de validarCampo para cada campo
         case 'usuario':
             validarCampo(expresiones.usuario,e.target,'usuario');
          break; 
@@ -42,7 +50,7 @@ const validarCampo=(expresion,input,campo)=>{
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');  //Seleccionamos varios elementos(un id y una etiqueta html) usando querySelector para AGREGAR la palomita verde
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');  //Seleccionamos varios elementos(un id y una etiqueta html) usando querySelector para AGREGAR la palomita verde
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');   
-        
+        campos[campo]=true;     //se cambia de false a true
     }
     else{       //si es falso se agrega la clase del grupo incorrecto que lo convierte en rojo
         document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto'); //selecciona el id del grupo__usuario y agrega la cruz roja por medio de la clase
@@ -50,7 +58,7 @@ const validarCampo=(expresion,input,campo)=>{
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-
+        campos[campo]=false;    
     }
 
 }
@@ -64,7 +72,7 @@ const validarPassword2=()=>{
             document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
             document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
             document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-                        
+            campos[password]=false;    
             
         }
         else{
@@ -73,21 +81,9 @@ const validarPassword2=()=>{
             document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
             document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
             document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-
+            campos[password]=true;
         }
 }
-
-/*
-
-const campos={
-    usuario: false,
-    nombre: false,
-    password: false,
-   
-    correo: false,
-    telefono: false
-}
-*/
 
 
 inputs.forEach((input)=>{
@@ -96,8 +92,26 @@ inputs.forEach((input)=>{
 })
 
 
-formulario.addEventListener('submit',(e)=>{
+formulario.addEventListener('submit',(e)=>{  //aquí se validará que todos los campos estén en true para que se pueda enviar el formulario
      e.preventDefault();
+     const terminos=document.getElementById('terminos');
+
+     if (campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked) { //si todos los campos son true
+        formulario.reset();     //formatea el formulario después de enviarse
+        //selecciona el mensaje de exito al resetear el formulario
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+           //sale el mensaje de exito despues de tanto milisegundos
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+
+        }, 3000);
+        // remueve todos los iconos
+        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono)=>{
+            icono.classList.remove('formulario__grupo-correcto');
+        })
+    }
+
+   
 
 })
 
